1652a23ff68ec6ef5496af726650f59aa29a9b21
DT_Suriya <suriya.krishnakumar@dispatchtrack.com>
2024-07-19 14:32:52 +0530

    team created

---
+++ db/migrate/20240719090052_create_teams.rb
@@ -0,0 +1,13 @@
+class CreateTeams < ActiveRecord::Migration[7.1]
+  def change
+    create_table :teams do |t|
+      t.string :name
+      t.string :country
+      t.integer :founded
+      t.text :description
+
+      t.timestamps
+    end
+    add_index :teams, :name, unique: true
+  end
+end
